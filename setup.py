62c2149a76100d49a48dad3950cdce373a9b2232
Shawn Furyan <shawn@furyan.co>
2017-01-14 17:30:26 -0600

    Added setuptools support with a minimal setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='diceware_prng',
+    version='0.01',
+    py_modules=['diceware_prng'],
+    install_requires=[
+        'Pandas',
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        diceware_prng=diceware_prng:passhprase
+    ''',
+)
+
