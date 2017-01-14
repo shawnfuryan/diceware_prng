d94b6b9608e113ee3c1e8920e448e95961a31c8a
Shawn Furyan <shawn@furyan.co>
2017-01-14 16:30:40 -0600

    Added an if __name__ == '__main__' check to support potential use as a library later

--- diceware_prng.py
+++ diceware_prng.py
@@ -3,21 +3,25 @@
 
 import pandas as pd
 import random
-import pprint
+import click
 
-r = random.SystemRandom()
-def rand_idx(end, start=0):
-    return r.randint(start,end)
+def passphrase():
+    r = random.SystemRandom()
+    def rand_idx(end, start=0):
+        return r.randint(start,end)
 
-filename = 'wordlist.csv'
-words_df = pd.read_csv(filename, index_col=0)
-def get_passphrase(num_words=6):
-    passphrase = []
-    for word in range(0,num_words):
-        word = words_df['word'][rand_idx(words_df.index[-1])]
-        passphrase.append(word)
-    return passphrase
+    filename = 'wordlist.csv'
+    words_df = pd.read_csv(filename, index_col=0)
+    def get_passphrase(num_words=6):
+        phrase = []
+        for word in range(0,num_words):
+            word = words_df['word'][rand_idx(words_df.index[-1])]
+            phrase.append(word)
+        return phrase
 
 
-passphrase = get_passphrase(10)
-print('%s' % ' '.join(map(str, passphrase)))
+    passphrase = get_passphrase(10)
+    print('%s' % ' '.join(map(str, passphrase)))
+
+if __name__ == '__main__':
+    passphrase()
