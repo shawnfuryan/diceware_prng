7bbf58f135ce3c3e2869f591433d0030bc0a369c
Shawn Furyan <shawn@furyan.co>
2017-01-14 17:16:19 -0600

    Added '--count' and '--help' CLI Options
    Grafted on a basic Click module interface.
    
    Manually added --count, which lets the user designate the
    desired number of words in the passphrase at the command line
    
    --help comes for free from the click module

--- diceware_prng.py
+++ diceware_prng.py
@@ -5,14 +5,16 @@ import pandas as pd
 import random
 import click
 
-def passphrase():
+@click.command()
+@click.option('--count', default=6, help='Number of words in passphrase')
+def passphrase(count):
     r = random.SystemRandom()
     def rand_idx(end, start=0):
         return r.randint(start,end)
 
     filename = 'wordlist.csv'
     words_df = pd.read_csv(filename, index_col=0)
-    def get_passphrase(num_words=6):
+    def get_passphrase(num_words):
         phrase = []
         for word in range(0,num_words):
             word = words_df['word'][rand_idx(words_df.index[-1])]
@@ -20,7 +22,7 @@ def passphrase():
         return phrase
 
 
-    passphrase = get_passphrase(10)
+    passphrase = get_passphrase(count)
     print('%s' % ' '.join(map(str, passphrase)))
 
 if __name__ == '__main__':
