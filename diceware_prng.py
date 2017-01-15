9d1b26641afdbafbb77957e2ccfd0e6033c3a08a
Shawn Furyan <shawn@furyan.co>
2017-01-14 20:56:41 -0600

    Fleshed out the user interface
    Added support for -h, instead of just --help
    
    change the --count option to --wordcount, then added a backup user
    prompt for when the user doesn't use the use this option. A case
    could be made for using a positional argument instead, but I'm
    pretty happy with this UX flow as it allows for quick execution
    for experienced users as well as discoverability for novices.
    
    Added a program description to the help prompt

--- diceware_prng.py
+++ diceware_prng.py
@@ -5,9 +5,31 @@ import random
 import click
 import json
 
-@click.command()
-@click.option('--count', default=6, help='Number of words in passphrase')
-def passphrase(count):
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+#@click.option('--count', default=6, help='Number of words in passphrase')
+@click.option('--wordcount', type=int,  prompt=True, help='Number of words in passphrase')
+def passphrase(wordcount):
+    """
+        diceware_prng.py: Generate strong passphrases without the dice.
+
+        Diceware_PRNG uses the excellent, nearly 8000 word strong Diceware dictionary
+        (see: diceware.org) and the cryptographically secure random number genrator
+        provided by many operating systems* (like Mac OSX, Windows 7+, Debian/Unbuntu/Mint,
+        Redhat/Fedora/CentOS, Arch/Manjaro, BSD, Solaris) to generate strong passphrases.
+
+        Each additional word int he passphrase adds 12.5 bits of entropy, so scale your
+        wordcount appropriately. The recommended minimum wordcount is 6, which yields
+        ~75 bits of entropy. A 10 word passphrase offers ~125 bits of entoropy which
+        should not be brute-forceable in the forseeable future.
+
+        *note: If not using one of these Operating Systems, it is up to the user to ensure
+        that their system provides a cryptographically secure random number generator
+        (usually /dev/urandom) that is recognized by python's os.urandom package. Using
+        this script in such an environment may yield insecure passwords. 
+
+    """
     r = random.SystemRandom()
     def rand_idx(end, start=0):
         return r.randint(start,end)
@@ -25,7 +47,7 @@ def passphrase(count):
         return phrase
 
 
-    passphrase = get_passphrase(count)
+    passphrase = get_passphrase(wordcount)
     print('%s' % ' '.join(map(str, passphrase)))
 
 if __name__ == '__main__':
