67470f8d3e4441f9e9df0aae72942cc5290d5e65
Shawn Furyan <shawn@furyan.co>
2017-01-15 03:24:48 -0600

    Refactored passphrase() and added basic err handling

--- diceware_prng.py
+++ diceware_prng.py
@@ -7,10 +7,22 @@ import json
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+RNG = random.SystemRandom()
+def rand_idx(end, start=0):
+    return RNG.randint(start,end)
+
+def get_passphrase(num_words, wordlist):
+    phrase = []
+    wordlist_last_idx = len(wordlist)-1
+    for word in range(0,num_words):
+        word = wordlist[rand_idx(wordlist_last_idx)]
+        phrase.append(word)
+    return phrase
+
+
 @click.command(context_settings=CONTEXT_SETTINGS)
-#@click.option('--count', default=6, help='Number of words in passphrase')
 @click.option('--wordcount', type=int,  prompt=True, help='Number of words in passphrase')
-def passphrase(wordcount):
+def passphrase(wordcount=6):
     """
         diceware_prng.py: Generate strong passphrases without the dice.
 
@@ -30,25 +42,28 @@ def passphrase(wordcount):
         this script in such an environment may yield insecure passwords. 
 
     """
-    r = random.SystemRandom()
-    def rand_idx(end, start=0):
-        return r.randint(start,end)
+
+    if wordcount < 6:
+        click.echo('ERROR: Diceware passphrases less than 6 words long are no longer '+ 
+                   'considered strong.', err=True)
+        click.echo('       Please choose a higher wordcount.', err=True)
+        exit() 
+    if wordcount > 1000:
+        click.echo('ERROR: Your enthusiasm is appreciated, but that many words is on the '+
+                   'verge of being difficult to memorize.', err=True)
+        click.echo('       Please choose a smaller wordcount.', err=True)
+        exit()
+        
 
     filename = 'wordlist.json'
     with open('wordlist.json') as f:
-        wordsl = json.load(f)
-    wordsl_last_idx = len(wordsl)-1
-    ##### words_df = pd.read_csv(filename, index_col=0)
-    def get_passphrase(num_words):
-        phrase = []
-        for word in range(0,num_words):
-            word = wordsl[rand_idx(wordsl_last_idx)]
-            phrase.append(word)
-        return phrase
+        wordlist = json.load(f)
 
+    passphrase = get_passphrase(wordcount, wordlist)
+    #print('%s' % ' '.join(map(str, passphrase)))
+    click.echo('%s' % ' '.join(map(str, passphrase)))
 
-    passphrase = get_passphrase(wordcount)
-    print('%s' % ' '.join(map(str, passphrase)))
+    return None
 
 if __name__ == '__main__':
     passphrase()
