a23328d645e9719fca2d414dcee792740d6b0014
Shawn Furyan <shawn@furyan.co>
2017-01-15 03:59:55 -0600

    Updated --help Description

--- diceware_prng.py
+++ diceware_prng.py
@@ -31,15 +31,26 @@ def passphrase(wordcount=6):
         provided by many operating systems* (like Mac OSX, Windows 7+, Debian/Unbuntu/Mint,
         Redhat/Fedora/CentOS, Arch/Manjaro, BSD, Solaris) to generate strong passphrases.
 
-        Each additional word int he passphrase adds 12.5 bits of entropy, so scale your
-        wordcount appropriately. The recommended minimum wordcount is 6, which yields
-        ~75 bits of entropy. A 10 word passphrase offers ~125 bits of entoropy which
+        Each additional word in the passphrase adds around 12.5 bits of entropy, so scale
+        your wordcount accordingly. The recommended minimum wordcount is 6, which yields
+        ~75 bits of entropy. A 10 word passphrase offers ~125 bits of entropy which
         should not be brute-forceable in the forseeable future.
+
+        Admittedly, this application is somewhat counter to the ideals of diceware.org,
+        if you really need very high assurance that the random number generation system
+        used to generate your passphrase has not been tampered with, you should still use
+        the dice based system described at diceware.org. This program is intended for 
+        lower security situations where convenience is more important than strict 
+        operational security.  Also, this code has not been audited by security
+        professionals, so use at your own risk. The authors of this program disclaim all
+        liability associated with the use of this software.
 
         *note: If not using one of these Operating Systems, it is up to the user to ensure
         that their system provides a cryptographically secure random number generator
-        (usually /dev/urandom) that is recognized by python's os.urandom package. Using
-        this script in such an environment may yield insecure passwords. 
+        (usually /dev/urandom) that is compatible with python's os.urandom package. Using
+        this script in such an environment may yield insecure passwords. This program
+        has not necessarily been tested (recently or ever) on all of the listed Operating
+        Systems.
 
     """
 
